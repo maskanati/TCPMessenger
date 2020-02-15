965c6418976f28eadd4dad3146582ab62d75a6f4
S.Maskanati <S.Maskanati@Home>
2020-02-15 08:40:34 +0330

    replace while-loops with recursive methods in the server

--- PeekageMessenger.ServiceHost.Server/PeekageMessengerServer.cs
+++ PeekageMessenger.ServiceHost.Server/PeekageMessengerServer.cs
@@ -15,62 +15,79 @@ using PeekageMessenger.Tools.Notification;
 
 namespace PeekageMessenger.ServiceHost.Server
 {
-    public class PeekageMessengerServer 
+    public class PeekageMessengerServer
     {
         private readonly TcpListener _tcpListener;
         private readonly INotification _notification;
 
         public PeekageMessengerServer(string ipAddress, int port)
         {
-            _notification=new ConsoleNotification();
+            _notification = new ConsoleNotification();
             _tcpListener = new TcpListener(IPAddress.Parse(ipAddress), port);
         }
+
         public async Task Run()
         {
             _tcpListener.Start();
             _notification.Info("Peekage", "Server started!");
             _notification.Notify("Server", "Waiting for a connection...");
-            do
-            {
-                var tcpClient = await _tcpListener.AcceptTcpClientAsync().ConfigureAwait(false);
-                _notification.Warning("Server", $"Client{tcpClient.GetId()} connected!");
 
-                new Thread(async () => { await ReplyToClientRequests(tcpClient); }).Start();
-            } while (true);
+            await AcceptNewClient();
         }
 
-        private async Task ReplyToClientRequests(TcpClient tcpClient)
+        private async Task AcceptNewClient()
+        {
+            var tcpClient = await _tcpListener.AcceptTcpClientAsync().ConfigureAwait(false);
+            _notification.Warning("Server", $"Client{tcpClient.GetId()} connected!");
+
+            _ = Task.Run(async () => await HandleClient(tcpClient));
+
+            await AcceptNewClient();
+        }
+
+        private async Task HandleClient(TcpClient tcpClient)
         {
             var clientId = tcpClient.GetId();
-
+            
             var responseSender = new ResponseSender(tcpClient);
-            ReplyResult connectionState = ReplyResult.StillConnected;
-            do
-            {
-                string message = string.Empty;
-
-                try { message = await tcpClient.ReadMessageAsync();}
-                catch (IOException exception)
-                {
-                    //When the connected clients console window closing without 'Bye' command
-                    break;
-                }
-
-                new Thread(async () =>
-                {
-                    _notification.Info($"Client {tcpClient.GetId()} said", message);
-                    var strategy = new ResponseStrategyFactory(responseSender).Create(message);
-                    connectionState = await strategy.Reply();
-                    _notification.Success("Server Replied", strategy.Message);
-                }).Start();
-
-                FluentHelper.WaitFor(200).Milliseconds();
-
-            } while (true && connectionState==ReplyResult.StillConnected);
+            
+            await HandelNewMessage(tcpClient, responseSender);
 
             _notification.Error("Server", $"Client{clientId} dropped out");
         }
 
+        private async Task HandelNewMessage(TcpClient tcpClient, ResponseSender responseSender)
+        {
+            string message = await GetNewMessage(tcpClient);
+            if (message == null) return;
+
+            _notification.Info($"Client {tcpClient.GetId()} said", message);
+
+            _ = Task.Run(() => ReplyToMessage(responseSender, message));
+
+            await HandelNewMessage(tcpClient, responseSender);
+        }
+
+        private async Task<ReplyResult> ReplyToMessage(ResponseSender responseSender, string message)
+        {
+            var strategy = new ResponseStrategyFactory(responseSender).Create(message);
+            var connectionState = await strategy.Reply();
+            _notification.Success("Server Replied", strategy.Message);
+            return connectionState;
+        }
+
+        private async Task<string> GetNewMessage(TcpClient tcpClient)
+        {
+            try { 
+                return await (Task.Run(() => tcpClient.ReadMessageAsync()));
+            }
+            catch(IOException ex)
+            {
+                //When the connected clients console window closing with/without 'Bye' command
+            }
+            return null;
+        }
+
     }
-    
+
 }
